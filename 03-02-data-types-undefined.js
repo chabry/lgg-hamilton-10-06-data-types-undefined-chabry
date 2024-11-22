4e038ba3f13c2a15b1e200fca1ad4afc59084193
Chabry <chabry01@gmail.com>
2024-11-22 14:28:07 +0100

    Exercice résolu

--- 03-02-data-types-undefined.js
+++ 03-02-data-types-undefined.js
@@ -3,21 +3,27 @@
 // Function 1
 function returnUndefined() {
   // Your code here
+  return undefined
 }
 
 // Function 2
 function undefinedVariable() {
   // Your code here
+  let undefinedVar
+  return undefinedVar
 }
 
 // Function 3
 function checkType(param) {
   // Your code here
+  return typeof(param)
 }
 
 // Function 4
 function returnUndefinedVar() {
   // Your code here
+  let undefinedVar
+  return undefinedVar
 }
 
 module.exports = {
